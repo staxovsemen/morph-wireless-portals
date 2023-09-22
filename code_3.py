4595b90367bc4700818b23f940f55dca7d1570f5
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-09-22 12:14:34 -0400

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,46 @@
+def main():
+
+    return data
+    for item in data:
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def generate_random_data():
+    main()
+
+
+    data = generate_random_data()
+if __name__ == "__main__":
+    for (let i = 0; i < 10; i++) {
+
+    return data;
+function main() {
+
+
+    data.forEach(item => {
+}
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+main();
+    }
+function generateRandomData() {
+    const data = [];
+    });
+    const data = generateRandomData();    data.forEach(item => {
+
+    return data;
+}
+function generateRandomData() {
+    }
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = generateRandomData();
+    });
+function main() {
+        console.log("Random Number: " + item);
+
+main();
+    const data = [];
+}
